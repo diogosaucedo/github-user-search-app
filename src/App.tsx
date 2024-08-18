7d88a70fa429a8edafe31a99552042fb96307c4b
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-08-18 18:12:24 -0400

    feat: exporta componentes e adiciona no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Header, Search, Profile } from "@components";
+import { Header, Search, Profile, ProfileList } from "@components";
+import { useState } from "react";
+import { User } from "./models";
 
 export default function App() {
+  const [user, setUser] = useState<User>();
+  const [favorites, setFavorites] = useState<User[]>([]);
+
   return (
     <div className="w-full max-w-3xl h-fit flex flex-col gap-4">
       <Header />
-      <Search />
-      <Profile />
+      <Search setUser={setUser} />
+      {user ? (
+        <Profile
+          user={user}
+          favorites={favorites}
+          setFavorites={setFavorites}
+        />
+      ) : (
+        <h1 className="text-gray-100">Nothing to show</h1>
+      )}
+      <h1 className="text-gray-200 text-xl">Saved Profiles</h1>
+      <ProfileList favorites={favorites} setFavorites={setFavorites} />
     </div>
   );
 }
