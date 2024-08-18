e5bbeae64a0d83dab288de2ec564c4cf5587ff6c
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-08-18 19:21:03 -0400

    refactor: trata lista vazia

--- src/components/ProfileList/index.tsx
+++ src/components/ProfileList/index.tsx
@@ -12,22 +12,26 @@ const ProfileList = ({ favorites, setFavorites }: ProfileListProps) => {
 
   return (
     <div className="flex-col w-full p-8 bg-slate-700 rounded-lg flex gap-4">
-      {favorites.map((user) => (
-        <div className="flex gap-4 bg-gray-900 p-2 rounded-lg w-full justify-between items-center">
-          <img
-            className="w-16 rounded-full"
-            src={user.avatar_url}
-            alt={user.name}
-          />
-          <h2 className="text-gray-200 text-xl">{user.name}</h2>
-          <button
-            onClick={() => removeFavorite(user.login)}
-            className="bg-red-600 rounded-lg h-12 w-20 text-gray-300"
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      {favorites.length != 0 ? (
+        favorites.map((user) => (
+          <div className="flex gap-4 bg-gray-900 p-2 rounded-lg w-full justify-between items-center">
+            <img
+              className="w-16 rounded-full"
+              src={user.avatar_url}
+              alt={user.name}
+            />
+            <h2 className="text-gray-200 text-xl">{user.name}</h2>
+            <button
+              onClick={() => removeFavorite(user.login)}
+              className="bg-red-600 rounded-lg h-12 w-20 text-gray-300"
+            >
+              Delete
+            </button>
+          </div>
+        ))
+      ) : (
+        <h1 className="text-gray-100">No profile saved</h1>
+      )}
     </div>
   );
 };
