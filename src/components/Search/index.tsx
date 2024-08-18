f9e0dde0c656ecce8b84cd9beceb4ed7d42522d7
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-08-18 18:10:30 -0400

    feat: adiciona fetch para api

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,13 +1,47 @@
-const Search = () => {
+import { useState } from "react";
+import { User } from "../../models";
+
+// Definindo a interface para as props
+interface SearchProps {
+  setUser: (user: User | undefined) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ setUser }) => {
+  // fetch user by username with github API
+  const fetchUserByUsername = async (
+    username: string
+  ): Promise<User | undefined> => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      return undefined;
+    }
+  };
+
+  const handleClick = async () => {
+    if (!username) return;
+    const user = await fetchUserByUsername(username);
+    setUser(user); // Agora você pode chamar setUser aqui
+  }
+
+  const [username, setUsername] = useState<string>();
+
   return (
     <div className="w-full h-14 bg-slate-700 rounded-lg flex items-center px-2">
       <img className="px-2" src="./icon-search.svg" alt="Icone de busca" />
       <input
+        onChange={(e) => setUsername(e.currentTarget.value)}
         className="w-full h-11 bg-transparent outline-none"
         type="text"
         placeholder="Search for developers..."
       />
-      <button className="text-slate-100 bg-blue-700 w-24 h-11 rounded-lg">
+      <button onClick={handleClick} className="text-slate-100 bg-blue-700 w-24 h-11 rounded-lg">
         Search
       </button>
     </div>
